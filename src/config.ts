7c541e83d70ec8da6fbce14a8663d1fcf4d74248
Rizumu Ayaka <rizumu@ayaka.moe>
2026-06-16 02:59:06 +0900

    fix: point contribution link to master branch and add repo metadata

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 export const repoUrl = 'https://github.com/LittleSound/techevent-cn'
 
 /** Link contributors land on when they want to add an event. */
-export const newEventUrl = `${repoUrl}/blob/main/CONTRIBUTING.md`
+export const newEventUrl = `${repoUrl}/blob/master/CONTRIBUTING.md`
 
 /** Published iCalendar feed for subscription. */
 export const calendarUrl = '/events.ics'
